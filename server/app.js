558dd4eb7a288264fb020e7d8c7a30845bbb49e1
Nitin Kaveriappa Udiyanda Muthanna <nitinkaveriappa@users.noreply.github.com>
2020-08-25 13:30:03 +0530

    Update

--- server/app.js
+++ server/app.js
@@ -82,14 +82,27 @@ const authCheck = (req, res, next) => {
 // if it's already login, send the profile response,
 // otherwise, send a 401 response that the user is not authenticated
 // authCheck before navigating to home page
-app.get('/*', authCheck, (req, res, next) => {
-  console.log('User Successfully Authenticated');
-  next();
-});
+// app.get('/*', authCheck, (req, res, next) => {
+//   console.log('User Successfully Authenticated');
+//   next();
+// });
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
